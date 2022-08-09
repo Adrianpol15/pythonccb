7cbe8ea10c1e6f9b0965ddbc2b4df068855a900e
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:30:41 -0600

    Create mayormenoriguales.py

---
+++ mayormenoriguales.py
@@ -0,0 +1,9 @@
+#mayormenoriguales
+n1 = int(input("Ingrese un numero: "))
+n2 = int(input("Ingrese otro numero: "))
+if n1<n2:
+  print("Es menor es el primer numero")
+elif n2<n1:
+  print("Es menor es el segundo numero")
+elif n2==n1:
+  print("Ingrese otros valores estos son iguales")
