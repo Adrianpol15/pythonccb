68808cab6fbaa4084ea1b00953ec12791bd0a0ad
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:16:38 -0600

    Create potencias.py

---
+++ potencias.py
@@ -0,0 +1,5 @@
+#Potencias
+n1=float(input("Ingrese el primer numero: "))
+n2=float(input("Ingrese el segundo numero: "))
+n3= n1**n2
+print(n3, "es el resultado del primer numero con la potencia del segundo numero: ")
