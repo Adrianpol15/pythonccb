ec57e7e17d3e4d3ca60cbbb138e4322d78d3e629
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:05:25 -0600

    Create holachepe.py

---
+++ holachepe.py
@@ -0,0 +1,32 @@
+Este programa dice "Hola chepe" y aparte hace un par de operaciones.
+print("Hola Chepe")
+
+print("Esto es una suma")
+n1=5
+n2=2
+res=n1+n2
+print(res)
+
+print("Esto es una resta")
+n1=167
+n2=69
+res=n1-n2
+print(res)
+
+print("Esto es una multiplicacion")
+n1=12
+n2=24
+res=n1*n2
+print(res)
+
+print("Esto es una Division")
+n1=20
+n2=5
+res=n1/n2
+print(res)
+
+print("Esto es un Modulo")
+n1=200
+n2=6
+res=n1%n2
+print(res)
