b07641de3ab666bac78642cdc606d6715112a409
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:29:35 -0600

    Create menorque.py

---
+++ menorque.py
@@ -0,0 +1,7 @@
+#menor qeu el primer numero
+n1 = int(input("Ingrese un numero: "))
+n2 = int(input("Ingrese otro numero: "))
+if n1<n2:
+  print("Es menor el primer numero")
+elif n2<n1:
+  print("Es menor el segundo numero")
