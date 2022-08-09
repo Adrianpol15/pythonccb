b0e0f0e08107acf630eb84b948f03ff58c60f95e
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:15:14 -0600

    Create tocayos.py

---
+++ tocayos.py
@@ -0,0 +1,7 @@
+#Son tocayos
+n1=str(input("Ingrese el primer nombre sin mayusculas: "))   
+n2=str(input("Ingrese el segundo nombre sin mayusculas: "))
+if n1 == n2:
+ print("Son tocayos")
+else:
+ print("No son tocayos")
