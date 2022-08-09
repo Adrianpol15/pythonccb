4fbaa3972c7ede993d994e5767420100570eda95
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-09 13:31:57 -0600

    Create diassemanamensaje.py

---
+++ diassemanamensaje.py
@@ -0,0 +1,16 @@
+#Dias de la semana con mensaje.
+n1=str(input("Ingrese un numero de 1 a 7: "))
+if n1 == "1": 
+  print("Lunes: Que tengas un buen inicio de semana")
+elif n1 == "2":
+  print("Ingrese otro numero")
+elif n1 == "3":
+  print("Ingrese otro numero")
+elif n1 == "4":
+  print("Ingrese otro numero")
+elif n1 == "5":
+  print("Viernes: Es viernes y el cuerpo lo sabe.")
+elif n1 == "6":
+  print("Sabado: Hoy es dia para una maraton de peliculas.")
+elif n1 == "7":
+  print("Domimgo: Pasa tiempo con tu familia y disfruta.")
