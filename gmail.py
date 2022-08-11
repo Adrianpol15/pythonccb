ae851fba967bb5a482b3b856dc02762333d56514
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 12:15:57 -0600

    Create gmail.py

---
+++ gmail.py
@@ -0,0 +1,11 @@
+#gmail
+n1=str(input("Ingrese el primer nombre: "))
+n2=str(input("Ingrese el segundo nombre: "))
+m3=str(input("Ingrese el primer apellido: "))
+m4=str(input("Ingrese el segundo apellido: "))
+
+p1=(n1[0])
+p2=(n2[0])
+p3="@baco.com"
+t=p1+p2+"."+m3+m4+p3
+print(t)
