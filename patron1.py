54d79540582747e82625f6a5f0249ee1ef4c3fef
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 11:30:13 -0600

    Create patron1.py

---
+++ patron1.py
@@ -0,0 +1,8 @@
+#programa de un patron
+print("Programa Patron")
+
+for i in range ( 0, 10): #Si se pueden poner mas numeros
+ for n in range ( 0, i+1):
+  print ( "# ", end="" )
+ print()
+#Creo que lo tengo
