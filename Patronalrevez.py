ff0ebee5149e30cbbb066c6c7ac78334358d61c8
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 11:42:25 -0600

    Create Patronalrevez.py

---
+++ Patronalrevez.py
@@ -0,0 +1,12 @@
+#patron al revez
+print("Programa Patron")
+
+for i in range ( 0, 15): 
+ for n in range ( 0, i+1):
+  print ( "* ", end="" )
+ print()
+
+for n in range (0, 15):  
+ for i in range (0, i):
+   print("* ", end="" )
+ print()
