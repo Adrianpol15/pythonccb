8abdedf2584d3b90c863f06f103ee287363edfde
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 11:18:54 -0600

    Create listasfor.py

---
+++ listasfor.py
@@ -0,0 +1,12 @@
+#listas for
+frutas=["sandia","naranja","banano","fresa","maracuya","pitaya", "piña","mango","mango verde" ]
+for fruta in frutas:
+  print(fruta)
+
+
+
+
+
+
+for x in range(-100,0,10):
+ print(x)
