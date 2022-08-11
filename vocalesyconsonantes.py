02a149510d52a937aaf0f979f4470b6a20003773
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 12:18:39 -0600

    Create vocalesyconsonantes.py

---
+++ vocalesyconsonantes.py
@@ -0,0 +1,16 @@
+#Muestra cuales son consonates y vocales.
+z=str(input("Ingrese una palabra: "))
+
+x=z[0]
+if x == "a":
+  print("inicia con una vocal ")
+elif x == "e":
+  print("inicia con una vocal ")
+elif x == "i":
+  print("inicia con una vocal ")
+elif x == "o":
+  print("inicia con una vocal ")
+elif x == "u":
+  print("inicia con una vocal ")
+else:
+  print("inicia con una consonante ")
