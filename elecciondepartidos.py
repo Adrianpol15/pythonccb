c32c549b20b2d27a2de99a0b5fde6e9535431e78
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 11:08:16 -0600

    Create elecciondepartidos.py

---
+++ elecciondepartidos.py
@@ -0,0 +1,11 @@
+#Eleccion de partidos.
+print("Elecciones de partidos")
+par=str(input("Elija el partido cual usted vota: A. Rojo B. Verde C. Azul: "))
+if par == "A":
+  print("Su voto ha sido realizado para el partido Rojo ")
+elif par == "B":
+  print("Su voto ha sido realizado para el partido verde ")
+elif par == "C":
+  print("Su voto ha sido realizado para el partido azul ")
+else: 
+  print("Opcion erronea, vuelva a realizar el voto")
