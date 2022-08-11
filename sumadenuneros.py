5ee6cdbb12d4778f9821b9fd9cec14411a0bd660
Adrianpol15 <109166958+Adrianpol15@users.noreply.github.com>
2022-08-11 12:25:14 -0600

    Create sumadenuneros.py

---
+++ sumadenuneros.py
@@ -0,0 +1,7 @@
+#Suma de los numeros.
+numero=[]
+for i in range (0,10):
+  x=int(input("Ingrese un numeros hasta llegar a 10 numeros: "))
+  numero.append(x)
+print(((list([1]))+(list([9]))))
+print(((list([2]))+(list([8]))))
